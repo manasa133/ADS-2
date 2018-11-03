9df74a6be49ad227f2fba785262b39ac378a57ed
ManasaMona <manasaalisetty@gmail.com>
2018-11-03 10:36:39 +0530

    "check_style_score -> 0 of 12 passed. style score: 0/1 "

---
+++ Week-1Exam/ADS-2Week-1/Solution.java
@@ -0,0 +1,56 @@
+import java.util.*;
+class PageRank {
+	Digraph graph;
+	PageRank(Digraph g){
+		graph = g;
+	}
+
+}
+
+class WebSearch {
+
+}
+
+
+public class Solution {
+	public static void main(String[] args) {
+		// read the first line of the input to get the number of vertices
+		Scanner sc = new Scanner(System.in);
+		int vertices = Integer.parseInt(sc.nextLine());
+		Digraph graph = new Digraph(vertices);
+
+		// iterate count of vertices times
+		// to read the adjacency list from std input
+		// and build the graph
+
+
+		for(int i=0;i<vertices;i++){
+			String[] list = sc.nextLine().split(" ");
+			for(int j=1;j<list.length;j++){
+				graph.addEdge(Integer.parseInt(list[0]),Integer.parseInt(list[j]));
+			}
+
+		}
+
+
+		// Create page rank object and pass the graph object to the constructor
+		PageRank prObj =  new PageRank(graph);
+		System.out.println(graph);
+
+		// print the page rank object
+
+		// This part is only for the final test case
+
+		// File path to the web content
+		String file = "WebContent.txt";
+
+		// instantiate web search object
+		// and pass the page rank object and the file path to the constructor
+
+		// read the search queries from std in
+		// remove the q= prefix and extract the search word
+		// pass the word to iAmFeelingLucky method of web search
+		// print the return value of iAmFeelingLucky
+
+	}
+}
