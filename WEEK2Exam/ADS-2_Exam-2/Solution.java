46357838292982c581fbdd1bcb2c7090b74018d2
ManasaMona <manasaalisetty@gmail.com>
2018-11-10 10:46:31 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

--- WEEK2Exam/ADS-2_Exam-2/Solution.java
+++ WEEK2Exam/ADS-2_Exam-2/Solution.java
@@ -8,10 +8,10 @@ public class Solution {
 		Scanner sc = new Scanner(System.in);
 		int Vertices = Integer.parseInt(sc.nextLine());
 		int Edges = Integer.parseInt(sc.nextLine());
-		EdgeWeightedGraph G = new EdgeWeightedGraph(Vertices);
+		EdgeWeightedDigraph G = new EdgeWeightedDigraph(Vertices);
 		for(int i =0 ;i < Edges;i++){
 			String[] vw= sc.nextLine().split(" ");
-			Edge e = new Edge(Integer.parseInt(vw[0]),Integer.parseInt(vw[1]),
+			DirectedEdge e = new DirectedEdge(Integer.parseInt(vw[0]),Integer.parseInt(vw[1]),
 				Double.parseDouble(vw[2]));
 			G.addEdge(e);
 		}
@@ -29,6 +29,11 @@ public class Solution {
 			// First is the source and second is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+		String[] sd = sc.nextLine().split(" ");
+		int source =  Integer.parseInt(sd[0]);
+		int destination = Integer.parseInt(sd[1]);
+		DijkstraSP dsp = new DijkstraSP(G,source);
+		System.out.println(dsp.distTo(destination));
 			break;
 
 		case "ViaPaths":
